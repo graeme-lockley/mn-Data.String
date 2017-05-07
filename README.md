d0804e415f634fbd9d9f06b05a4e9b8afc2f6db6
Graeme Lockley <graemel@no9.co.za>
2017-05-07 09:00:37 +0200

    refactor: move the assumptions out into a test

--- README.md
+++ README.md
@@ -8,13 +8,6 @@ String => (==) :: String -> Bool
 
 Tests whether or not the parameter has the same value as `this`.
 
-#### Examples:
-
-```haskell
-of("Hello").$EQUAL$EQUAL(of("Hello")) == true
-of("Hello").$EQUAL$EQUAL(of("World")) == false
-```
-
 ### (!=)
 
 ```haskell
@@ -23,27 +16,12 @@ String => (!=) :: String -> Bool
 
 Tests whether or not the parameter has a different value to `this`.
 
-#### Examples:
-
-```haskell
-of("Hello").$NOT$EQUAL(of("World")) == true
-of("Hello").$NOT$EQUAL(of("Hello")) == false
-```
-
 ### (<=)
 
 ```haskell
 String => (<=) :: String -> Bool
 ```
 
-
-#### Examples:
-
-```haskell
-of("Hello").$LESS$EQUAL(of("World")) == true
-of("Hello").$LESS$EQUAL(of("Hello")) == true
-of("World").$LESS$EQUAL(of("Hello")) == false
-```
 
 ### (<)
 
@@ -52,28 +30,12 @@ String => (<) :: String -> Bool
 ```
 
 
-#### Examples:
-
-```haskell
-of("Hello").$LESS(of("World")) == true
-of("Hello").$LESS(of("Hello")) == false
-of("World").$LESS(of("Hello")) == false
-```
-
 ### (>=)
 
 ```haskell
 String => (>=) :: String -> Bool
 ```
 
-
-#### Examples:
-
-```haskell
-of("Hello").$GREATER$EQUAL(of("World")) == false
-of("Hello").$GREATER$EQUAL(of("Hello")) == true
-of("World").$GREATER$EQUAL(of("Hello")) == true
-```
 
 ### (>)
 
@@ -82,14 +44,6 @@ String => (>) :: String -> Bool
 ```
 
 
-#### Examples:
-
-```haskell
-of("Hello").$GREATER(of("World")) == false
-of("Hello").$GREATER(of("Hello")) == false
-of("World").$GREATER(of("Hello")) == true
-```
-
 ### compare
 
 ```haskell
@@ -97,14 +51,6 @@ String => compare :: String -> Data.Ordered.Ordering
 ```
 
 
-#### Examples:
-
-```haskell
-of("Hello").compare(of("World")) == Ordered.LT
-of("Hello").compare(of("Hello")) == Ordered.EQ
-of("World").compare(of("Hello")) == Ordered.GT
-```
-
 ### max
 
 ```haskell
@@ -112,28 +58,12 @@ String => max :: String -> String
 ```
 
 
-#### Examples:
-
-```haskell
-of("Hello").max(of("World")) == of("World")
-of("Hello").max(of("Hello")) == of("Hello")
-of("World").max(of("Hello")) == of("World")
-```
-
 ### max
 
 ```haskell
 String => max :: String -> String
 ```
 
-
-#### Examples:
-
-```haskell
-of("Hello").min(of("World")) == of("Hello")
-of("Hello").min(of("Hello")) == of("Hello")
-of("World").min(of("Hello")) == of("Hello")
-```
 
 ### at
 
@@ -142,28 +72,12 @@ String => at :: Data.Int -> Maybe String
 ```
 
 
-#### Examples:
-
-```haskell
-of("Hello").at(Int.of(0)) == Maybe.Just(of("H"))
-of("Hello").at(Int.of(1)) == Maybe.Just(of("e"))
-of("Hello").at(Int.of(5)) == Maybe.Nothing
-```
-
 ### indexOfFrom
 
 ```haskell
 String => indexOfFrom :: String -> Int -> Maybe Int
 ```
 
-
-#### Examples:
-
-```haskell
-of("hello").indexOfFrom(of("world"))(Int.of(2)) == Maybe.Nothing
-of("hello").indexOfFrom(of("hello"))(Int.of(2)) == Maybe.Nothing
-of("hello").indexOfFrom(of("ll"))(Int.of(2)) == Maybe.Just(Int.of(2))
-```
 
 ### indexOf
 
@@ -172,27 +86,12 @@ String => indexOf :: String -> Maybe Int
 ```
 
 
-#### Examples:
-
-```haskell
-of("hello").indexOf(of("world")) == Maybe.Nothing
-of("hello").indexOf(of("hello")) == Maybe.Just(of(0))
-of("hello").indexOf(of("ll")) == Maybe.Just(of(2))
-```
-
 ### length
 
 ```haskell
 String => length :: () -> Int
 ```
 
-
-#### Examples:
-
-```haskell
-of("").length() == Int.of(0)
-of("hello").length() == Int.of(5)
-```
 
 ### startsWith
 
@@ -201,13 +100,6 @@ String => startsWith :: String -> Bool
 ```
 
 
-#### Examples:
-
-```haskell
-of("hello").startsWith(of("he")) == true
-of("hello").startsWith(of("wor")) == false
-```
-
 ### substring
 
 ```haskell
@@ -215,24 +107,12 @@ String => substring :: Int -> Int -> String
 ```
 
 
-#### Examples:
-
-```haskell
-of("hello").substring(Int.of(1))(Int.of(3)) == of("el")
-```
-
 ### substringFrom
 
 ```haskell
 String => substringFrom :: Int -> String
 ```
 
-
-#### Examples:
-
-```haskell
-of("hello").substringFrom(Int.of(1)) == of("ello")
-```
 
 ### trim
 
@@ -249,13 +129,6 @@ String => replace :: String -> String
 
 Replaces the first occurrence of the first argument with the second argument.
 
-#### Examples:
-
-```haskell
-of("hello").replace(of("l"))(of("L")) == of("heLlo")
-of("he**o").replace(of("*"))(of("=")) == of("he=*o")
-```
-
 ### replaceAll
 
 ```haskell
@@ -263,13 +136,6 @@ replaceAll :: String -> String -> String
 ```
 
 Replaces all occurrence of the first argument with the second argument.
-
-#### Examples:
-
-```haskell
-of("hello").replaceAll(of("l"))(of("L")) == of("heLLo")
-of("he**o").replaceAll(of("*"))(of("=")) == of("he==o")
-```
 
 ### (++)
 
@@ -279,14 +145,6 @@ String => (++) :: String -> String
 
 Concatenates two strings together to a create a new string.
 
-#### Examples:
-
-```haskell
-of("Hello").$PLUS$PLUS(of("World")) == of("HelloWorld")
-of("abc").show() == of("\"abc\"")
-of("a\"bc").show() == of("\"a\\\"bc\"")
-```
-
 
 ## Dependencies
 
@@ -295,3 +153,4 @@ of("a\"bc").show() == of("\"a\\\"bc\"")
 * [Data.Parity (1.0.0)](https://github.com/graeme-lockley/mn-Data.Parity)
 * [Data.Ordered (1.0.0)](https://github.com/graeme-lockley/mn-Data.Ordered)
 * [Data.Show (1.0.0)](https://github.com/graeme-lockley/mn-Data.Show)
+* [Data.Int (1.0.1)](https://github.com/graeme-lockley/mn-Data.Int)
---
+++ Test/Data/StringTest.js
@@ -0,0 +1,99 @@
+const Assert = require("assert");
+const Unit = mrequire("core:Test.Unit:v1.0.0");
+
+const Int = mrequire("core:Data.Int:1.0.1");
+const Maybe = mrequire("core:Data.Maybe:1.2.0");
+const Ordered = mrequire("core:Data.Ordered:1.0.0");
+
+const $String = require("../../index.js");
+
+
+Unit.newSuite("Data.String")
+    .case("(==)", () => {
+        Assert.deepEqual($String.$String.of("Hello").$EQUAL$EQUAL($String.$String.of("Hello")), true);
+        Assert.deepEqual($String.$String.of("Hello").$EQUAL$EQUAL($String.$String.of("World")), false);
+    })
+    .case("(!=)", () => {
+        Assert.deepEqual($String.$String.of("Hello").$NOT$EQUAL($String.$String.of("World")), true);
+        Assert.deepEqual($String.$String.of("Hello").$NOT$EQUAL($String.$String.of("Hello")), false);
+    })
+    .case("(<=)", () => {
+        Assert.deepEqual($String.of("Hello").$LESS$EQUAL($String.of("World")), true);
+        Assert.deepEqual($String.of("Hello").$LESS$EQUAL($String.of("Hello")), true);
+        Assert.deepEqual($String.of("World").$LESS$EQUAL($String.of("Hello")), false);
+    })
+    .case("(<)", () => {
+        Assert.deepEqual($String.of("Hello").$LESS($String.of("World")), true);
+        Assert.deepEqual($String.of("Hello").$LESS($String.of("Hello")), false);
+        Assert.deepEqual($String.of("World").$LESS($String.of("Hello")), false);
+    })
+    .case("(>=)", () => {
+        Assert.deepEqual($String.of("Hello").$GREATER$EQUAL($String.of("World")), false);
+        Assert.deepEqual($String.of("Hello").$GREATER$EQUAL($String.of("Hello")), true);
+        Assert.deepEqual($String.of("World").$GREATER$EQUAL($String.of("Hello")), true);
+    })
+    .case("(>)", () => {
+        Assert.deepEqual($String.of("Hello").$GREATER($String.of("World")), false);
+        Assert.deepEqual($String.of("Hello").$GREATER($String.of("Hello")), false);
+        Assert.deepEqual($String.of("World").$GREATER($String.of("Hello")), true);
+    })
+    .case("compare", () => {
+        Assert.deepEqual($String.of("Hello").compare($String.of("World")), Ordered.LT);
+        Assert.deepEqual($String.of("Hello").compare($String.of("Hello")), Ordered.EQ);
+        Assert.deepEqual($String.of("World").compare($String.of("Hello")), Ordered.GT);
+    })
+    .case("max", () => {
+        Assert.deepEqual($String.of("Hello").max($String.of("World")), $String.of("World"));
+        Assert.deepEqual($String.of("Hello").max($String.of("Hello")), $String.of("Hello"));
+        Assert.deepEqual($String.of("World").max($String.of("Hello")), $String.of("World"));
+    })
+    .case("min", () => {
+        Assert.deepEqual($String.of("Hello").min($String.of("World")), $String.of("Hello"));
+        Assert.deepEqual($String.of("Hello").min($String.of("Hello")), $String.of("Hello"));
+        Assert.deepEqual($String.of("World").min($String.of("Hello")), $String.of("Hello"));
+    })
+    .case("at", () => {
+        Assert.deepEqual($String.of("Hello").at(Int.of(0)), Maybe.Just($String.of("H")));
+        Assert.deepEqual($String.of("Hello").at(Int.of(1)), Maybe.Just($String.of("e")));
+        Assert.deepEqual($String.of("Hello").at(Int.of(5)), Maybe.Nothing);
+    })
+    .case("indexFrom", () => {
+        Assert.deepEqual($String.of("hello").indexOfFrom($String.of("world"))(Int.of(2)), Maybe.Nothing);
+        Assert.deepEqual($String.of("hello").indexOfFrom($String.of("hello"))(Int.of(2)), Maybe.Nothing);
+        Assert.deepEqual($String.of("hello").indexOfFrom($String.of("ll"))(Int.of(2)), Maybe.Just(Int.of(2)));
+    })
+    .case("indexOf", () => {
+        Assert.deepEqual($String.of("hello").indexOf($String.of("world")), Maybe.Nothing);
+        Assert.deepEqual($String.of("hello").indexOf($String.of("hello")), Maybe.Just(of(0)));
+        Assert.deepEqual($String.of("hello").indexOf($String.of("ll")), Maybe.Just(of(2)));
+    })
+    .case("length", () => {
+        Assert.deepEqual($String.of("").length(), Int.of(0));
+        Assert.deepEqual($String.of("hello").length(), Int.of(5));
+    })
+    .case("startsWith", () => {
+        Assert.deepEqual($String.of("hello").startsWith($String.of("he")), true);
+        Assert.deepEqual($String.of("hello").startsWith($String.of("wor")), false);
+    })
+    .case("substring", () => {
+        Assert.deepEqual($String.of("hello").substring(Int.of(1))(Int.of(3)), $String.of("el"));
+    })
+    .case("substringFrom", () => {
+        Assert.deepEqual($String.of("hello").substringFrom(Int.of(1)), $String.of("ello"));
+    })
+    .case("replace", () => {
+        Assert.deepEqual($String.of("hello").replace($String.of("l"))($String.of("L")), $String.of("heLlo"));
+        Assert.deepEqual($String.of("he**o").replace($String.of("*"))($String.of("=")), $String.of("he=*o"));
+    })
+    .case("replaceAll", () => {
+        Assert.deepEqual($String.of("hello").replaceAll($String.of("l"))($String.of("L")), $String.of("heLLo"));
+        Assert.deepEqual($String.of("he**o").replaceAll($String.of("*"))($String.of("=")), $String.of("he==o"));
+    })
+    .case("(++)", () => {
+        Assert.deepEqual($String.of("Hello").$PLUS$PLUS($String.of("World")), $String.of("HelloWorld"));
+    })
+    .case("show", () => {
+        Assert.deepEqual($String.of("abc").show(), $String.of("\"abc\""));
+        Assert.deepEqual($String.of("a\"bc").show(), $String.of("\"a\\\"bc\""));
+    })
+;
